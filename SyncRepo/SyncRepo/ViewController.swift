05c2b4315c4e7c3950fc3c7861a78a7d001c242b
Tan (Tharin) T. Nguyen <ngthanhtan91@gmail.com>
2018-12-27 15:28:43 +0700

    [1] Adding new commit

--- SyncRepo/SyncRepo/ViewController.swift
+++ SyncRepo/SyncRepo/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     }
 
     func helloWorld() {
+        print("1 update")
         print("Hello world")
+        print("1 end update")
     }
     
     func goodbyeSyncIssues() {
